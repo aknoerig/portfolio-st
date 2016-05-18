e206b9d30e6c9bd0b762cf3d35a6e08ec79cc436
André Knörig <andre.knoerig@gmail.com>
2016-05-18 11:28:37 +0200

    hide lightroom access

--- sites/header.php
+++ sites/header.php
@@ -2,10 +2,10 @@
 
    		if(isset($_GET['s'])) {
 
-			$_set_hider = "";	  	
-			$_set_intro = " hider";	  	
+			$_set_hider = "";
+			$_set_intro = " hider";
 			$_set_logo = "-onstate";
-			$_set_overview = "";	  	 	
+			$_set_overview = "";
  			$_set_cuts_a = "";
 			$_set_wNavi = "";
 			$_set_lightroom_a = "";
@@ -14,13 +14,13 @@
  			$_set_cuts_a = "";
 			$_set_wNavi = "";
 			$_set_lightroom_a = "";
-						  		
+
    		}
 
    		if(!isset($_GET['s'])) {
-   		
-			$_set_hider = " class=\"hider\"";	  	
-			$_set_overview = " class=\"hiderOverview\"";	  	
+
+			$_set_hider = " class=\"hider\"";
+			$_set_overview = " class=\"hiderOverview\"";
 			$_set_intro = "";
 			$_GET['s'] = "books";
  			$_set_cuts_a = "";
@@ -28,11 +28,11 @@
 			$_set_lightroom_a = "";
 			$_set_book_a = "";
 			$_set_expand = "";
-		
+
 		}
 
    		if(isset($_GET['s']) AND $_GET['s'] == "books") {
-	  	
+
 			$_set_book_a = " class=\"active\"";
  			$_set_cuts_a = "";
 			$_set_wNavi = "";
@@ -40,11 +40,11 @@
 			$_set_expand = "";
 			$_set_mobileMenuCuts = "";
 			$_set_mobileMenuBooks = " class=\"flipMenuMobileActive\"";
-	  	
-		}	
+
+		}
 
    		if(isset($_GET['s']) AND $_GET['s'] == "cuts" ) {
-	  	
+
 			$_set_cuts_a = " class=\"active\"";
 			$_set_wNavi = "";
 			$_set_lightroom_a = "";
@@ -52,22 +52,22 @@
 			$_set_expand = "";
 			$_set_mobileMenuCuts = " class=\"flipMenuMobileActive\"";
 			$_set_mobileMenuBooks = "";
-	  	
-		}	
+
+		}
 
    		if(isset($_GET['s']) AND $_GET['s'] == "project") {
-	  	
+
 			$_set_dividerStretch = "Content";
  			$_set_cuts_a = "";
 			$_set_wNavi = "";
 			$_set_lightroom_a = "";
 			$_set_book_a = "";
 			$_set_expand = "";
-	  	
-		}	
+
+		}
 
    		if(!isset($_GET['s']) OR isset($_GET['list']) AND $_GET['list'] == "all") {
-	  	
+
 			$_set_toggleSub = " toggleSub";
 			$_set_enterHeight = " class=\"toggleContent\"";
  			$_set_cuts_a = "";
@@ -75,59 +75,59 @@
 			$_set_lightroom_a = "";
 			$_set_book_a = "";
 			$_set_expand = "";
-	  	
-		}	
+
+		}
 
    		if(isset($_GET['list']) AND $_GET['list'] == "all") {
-	  	
+
 			$_set_all_active = " class=\"active\"";
  			$_set_cuts_a = "";
 			$_set_wNavi = "";
 			$_set_lightroom_a = "";
 			$_set_book_a = "";
 			$_set_expand = "";
-	  	
-		}	
-		
+
+		}
+
    		if(isset($_GET['s']) AND $_GET['s'] == "lightroom" ) {
-	  	
+
 			$_set_lightroom_a = " class=\"active\"";
 			$_set_wNavi = " class=\"navi-white\"";
 			$_set_wSign = "_d";
  			$_set_cuts_a = "";
 			$_set_book_a = "";
 			$_set_expand = "ltr_expand";
-	  	
-		}	
+
+		}
 
    		if(isset($_GET['s']) AND $_GET['s'] == "login") {
-	  	
+
 			$_set_login = " class=\"login_mask\"";
-	  	
-		}	
+
+		}
 
 
 	$sql_info = "
-	
-			SELECT 
-			
+
+			SELECT
+
 				mobile
-				
+
 			FROM	about
-						
+
 		";
 
-    $result_info = mysql_query($sql_info) OR die("<pre>".$sql_info."</pre>".mysql_error()); 
+    $result_info = mysql_query($sql_info) OR die("<pre>".$sql_info."</pre>".mysql_error());
 	$row_info = mysql_fetch_assoc($result_info);
 
-		
+
 ?>
 
-	
-	<?php	
+
+	<?php
 		if ($detect->isMobile() && !$detect->isTablet()) {
 	?>
-	
+
 		<div id="topBarMobile">
 			<div id="topBarLogo">
 				<a href="/books/" /><img src="/img/Sabrina_Theissen_MobileLogo.svg" /></a>
@@ -143,8 +143,8 @@
 		?>
 
 	<div id="naviContainer"<?php echo "$_set_hider"; ?>>
-	
-		<ul<?php echo "$_set_wNavi"; ?>>	
+
+		<ul<?php echo "$_set_wNavi"; ?>>
 			<li><a href="/books/"<?php echo "$_set_book_a"; ?> title="Online portfolio of Berlin-based fashion photographer Sabrina Theissen. Among her clients are: Achtung, Bon, Feld, Hugo Boss, I love you, Interview (Russia), L'Officiel Hommes, Sleek, Under Current, Vogue (Germany)">Books</a></li>
 			<li class="books_timeout"><a href="/books/"<?php echo "$_set_book_a"; ?> title="Online portfolio of Berlin-based fashion photographer Sabrina Theissen. Among her clients are: Achtung, Bon, Feld, Hugo Boss, I love you, Interview (Russia), L'Officiel Hommes, Sleek, Under Current, Vogue (Germany)">Books</a></li>
 			<li><a href="/cuts/"<?php echo "$_set_cuts_a"; ?> title="Places, thoughts and experiments. Between here and there">Cuts</a></li>
@@ -156,9 +156,11 @@
 			}	else {
 			?>
 			<li class="unwhite set_timeout"><a href="#about" title="Sabrina Theissen is a German photographer. After living in London for a while she is now based in Berlin.">About</a></li>
-		<?php	
+		<?php
 			}
 	?>
+
+  <!--
 			<li class="set_timeout ltr_mobile <?php echo "$_set_expand"; ?>"><a href="/lightroom/"<?php echo "$_set_lightroom_a"; ?> title="Access the whole archive and create your personal selection.">Lightroom</a>
 	<?php
 	   		if(isset($_GET['s']) AND $_GET['s'] == "lightroom" ) {
@@ -167,24 +169,26 @@
 	<?php
 			}
 	?>
-			</li>			
+			</li>
+  -->
+
 			<li class="hide_unsub"><a href="#unsubscribe">Unsubscribe</a></li>
 			<li class="hide_unsub"><a href="#access">Redirect</a></li>
 		</ul>
-		
-	</div>		
+
+	</div>
 
 	<?php
-		}	
+		}
 		?>
-	
-	
-	
-	
+
+
+
+
 	<?php
 	   		if(isset($_GET['s']) AND $_GET['s'] == "lightroom" ) {
 			?>
-	
+
 	<div id="subNavLtr">
 		<ul>
 			<li></li>
@@ -199,12 +203,12 @@
 				<p id="couples">Couples</p><br />
 				<p id="objects">Objects</p>
 			</li>
-		</ul>	
+		</ul>
 	</div>
-	
+
 		<?php
 			}
 	?>
 
-	
+
 	<div id="info"></div>
--- sites/lightroom.php
+++ sites/lightroom.php
@@ -16,16 +16,16 @@
 
 			if(!isset($_SESSION['GO_access']))
 			   {
-			   
-			   echo "<meta http-equiv=\"refresh\" content=\"0; URL=http://www.sabrinatheissen.com/login/\">";
+
+			   echo "<meta http-equiv=\"refresh\" content=\"0; URL=/login/\">";
 
 			   }  else  {
-			   
-?> 
-			   
+
+?>
+
 			   <script>
 			   $(function() {
-					$("html, body").css({ "background-color" : "#1a1b20", "-webkit-transition" : "0.6s linear", "-moz-transition" : "0.6s linear", "-o-transition" : "0.6s linear", "transition" : "0.6s linear" },0);			   
+					$("html, body").css({ "background-color" : "#1a1b20", "-webkit-transition" : "0.6s linear", "-moz-transition" : "0.6s linear", "-o-transition" : "0.6s linear", "transition" : "0.6s linear" },0);
 					$(".navi-white").children("li").addClass("white-points");
 					$("#counter p").addClass("white-points");
 					$("#mark-books").hide();
@@ -41,65 +41,65 @@
 	<div id="overview"<?php echo "$_set_overview"; ?>>
 
 	<div id="ltb">
-	
+
 	<?php
-	
-	$sql_ltr_img = "SELECT 
-			
+
+	$sql_ltr_img = "SELECT
+
                             ID,
                             ID_a,
                             content_img,
                             recordListingID
-                    
-                    FROM 
-                                       
+
+                    FROM
+
                             img
-                            
+
                     WHERE ID_a != 'NULL'
-                                                        
+
                     ORDER BY recordListingID ASC
-                                                        
-           			";  
-           			
-    $result_ltr_img = mysql_query($sql_ltr_img) OR die("<pre>".$sql_ltr_img."</pre>".mysql_error()); 
-	while($row_ltr_img = mysql_fetch_assoc($result_ltr_img)) {	
-	
-		$sql_ltr_cat = "SELECT 
-			
+
+           			";
+
+    $result_ltr_img = mysql_query($sql_ltr_img) OR die("<pre>".$sql_ltr_img."</pre>".mysql_error());
+	while($row_ltr_img = mysql_fetch_assoc($result_ltr_img)) {
+
+		$sql_ltr_cat = "SELECT
+
                             ID,
                             ID_cat,
                             ID_2ndcat
-                    
-                    FROM 
-                                       
+
+                    FROM
+
                             lightbox
-                            
+
                     WHERE ID = '".$row_ltr_img['ID_a']."'
-                                                        
-           			";  
-           			
-    		$result_ltr_cat = mysql_query($sql_ltr_cat) OR die("<pre>".$sql_ltr_cat."</pre>".mysql_error()); 
+
+           			";
+
+    		$result_ltr_cat = mysql_query($sql_ltr_cat) OR die("<pre>".$sql_ltr_cat."</pre>".mysql_error());
 			$row_ltr_cat = mysql_fetch_assoc($result_ltr_cat);
-	
-	
+
+
 			echo "<div id=\"container\" class=\"cat".htmlentities($row_ltr_cat['ID_cat'], ENT_QUOTES)." cat".htmlentities($row_ltr_cat['ID_2ndcat'], ENT_QUOTES)."\"><p class=\"add\">ADD</p><p class=\"clear\">DROP</p><div class=\"reset\"></div><div class=\"clicked\"></div><img src=\"/cms/images/ltr_thumbs_sml/".htmlentities($row_ltr_img['content_img'], ENT_QUOTES)."\" /></div>\n";
-	
+
 	}
-	?>		
-				
+	?>
+
 			</div>
 
 		<div id="clear"></div>
-	
+
 	</div>
-	
+
 	<div id="counter"><p>Selected: <span>0</span></p></div>
 	<div id="topper_ltr"><a href="#top"><p>Top</p></a></div>
 	<div id="ltr_note_container"><p><?php if(isset($note)) { echo $note; } ?></p></div>
 	<div id="ltr_btn_container">
 		 <input type="submit" class="ltr_btn" value="Download" name="Download">
 	</div>
-	
-<?php } ?>	
+
+<?php } ?>
 
 </div>
