798e0e951f516aa30e476e2a9c9d4638ba4ff1d8
André Knörig <andre.knoerig@gmail.com>
2016-08-11 16:54:12 +0200

    make whole mobile thumb clickable

--- sites/books.php
+++ sites/books.php
@@ -378,7 +378,7 @@ if(isset($_GET['cat']) AND $_GET['cat'] == "all") {
 
 				}
 
-			echo "<div id=\"project\" class=\"book-item\"><a href=\"/books/".$pushCat."/".$row_items_list['ID']."/\" title=\"&nbsp;&rsaquo;".htmlentities($row_items_list['name'], ENT_QUOTES)."&lsaquo; for ".htmlentities($row_client_list['name'], ENT_QUOTES)."&nbsp;\"><img src=\"/cms/images/thumbs/".htmlentities($row_img['content_img'], ENT_QUOTES)."\" alt=\"Sabrina Theissen | N&deg;".htmlentities($row_items_list['recordListingID'], ENT_QUOTES)." &rsaquo;".htmlentities($row_items_list['name'], ENT_QUOTES)."&lsaquo; for ".htmlentities($row_client_list['name'], ENT_QUOTES)."\" /></a>";
+			echo "<div id=\"project\" class=\"book-item\"><a href=\"/books/".$pushCat."/".$row_items_list['ID']."/\" title=\"&nbsp;&rsaquo;".htmlentities($row_items_list['name'], ENT_QUOTES)."&lsaquo; for ".htmlentities($row_client_list['name'], ENT_QUOTES)."&nbsp;\"><img src=\"/cms/images/thumbs/".htmlentities($row_img['content_img'], ENT_QUOTES)."\" alt=\"Sabrina Theissen | N&deg;".htmlentities($row_items_list['recordListingID'], ENT_QUOTES)." &rsaquo;".htmlentities($row_items_list['name'], ENT_QUOTES)."&lsaquo; for ".htmlentities($row_client_list['name'], ENT_QUOTES)."\" />";
 
 				if ($detect->isMobile() && !$detect->isTablet()) {
 
@@ -391,7 +391,7 @@ if(isset($_GET['cat']) AND $_GET['cat'] == "all") {
 
 				}
 
-			echo "<h3>".htmlentities($row_client_list['name'], ENT_QUOTES)."</h3></div>\n";
+			echo "<h3>".htmlentities($row_client_list['name'], ENT_QUOTES)."</h3></a></div>\n";
 
 		}
 
